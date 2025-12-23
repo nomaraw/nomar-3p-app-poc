e54bc8918cc601bbee3c16555f020827da31ea34
nomaraw <54260353+nomaraw@users.noreply.github.com>
2025-12-23 07:03:43 -0600

    Delete src directory

--- src/sdk-entry.js
+++
@@ -1,6 +0,0 @@
-
-// Import the entire module as a namespace and export it as default.
-// The SDK object exposes .init(...) per AWS docs.
-import * as AmazonConnectApp from '@amazon-connect/app';
-
-export default AmazonConnectApp;
