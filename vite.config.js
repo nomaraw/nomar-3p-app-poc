b55be02bc33a51bd0879075fb988069245e02e70
nomaraw <54260353+nomaraw@users.noreply.github.com>
2025-12-23 06:23:01 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '', // leave as empty for project pages; we reference files with absolute GitHub Pages URL
   build: {
     lib: {
       entry: './src/sdk-entry.js',
-      name: 'AmazonConnectApp', // window.AmazonConnectApp in UMD
+      name: 'AmazonConnectApp',              // UMD global: window.AmazonConnectApp
       formats: ['umd', 'es'],
       fileName: (format) => `amazon-connect-app.${format}.js`,
     },
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
